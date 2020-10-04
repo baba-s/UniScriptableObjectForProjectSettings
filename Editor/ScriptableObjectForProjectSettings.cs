20c40d0954c09c70038487d624ddab04fd729fe1
baba_s <shotababa1206@gmail.com>
2020-10-04 19:11:54 +0900

    シーン遷移時にエラーが発生する不具合修正

--- Editor/ScriptableObjectForProjectSettings.cs
+++ Editor/ScriptableObjectForProjectSettings.cs
@@ -21,19 +21,24 @@ namespace Kogane
 		private static T m_instance;
 
 		//================================================================================
+		// プロパティ(static)
+		//================================================================================
+		private static string AssetPath => $"ProjectSettings/Kogane/{typeof( T ).Name}.asset";
+
+		//================================================================================
 		// 関数(static)
 		//================================================================================
 		/// <summary>
 		/// 指定された .asset から ScriptableObject のインスタンスを読み込んで返します
 		/// .asset が存在しない場合は ScriptableObject のインスタンスを新規作成して返します
 		/// </summary>
-		private static T CreateOrLoadFromAsset( string assetPath )
+		public static T GetInstance()
 		{
 			// 既にインスタンスを作成もしくは読み込み済みの場合はそれを返す
 			if ( m_instance != null ) return m_instance;
 
 			// .asset が存在しない場合はインスタンスを新規作成する
-			if ( !File.Exists( assetPath ) )
+			if ( !File.Exists( AssetPath ) )
 			{
 				m_instance = CreateInstance<T>();
 				return m_instance;
@@ -41,7 +46,7 @@ namespace Kogane
 
 			// .asset が存在する場合は .asset を読み込む
 			m_instance = InternalEditorUtility
-					.LoadSerializedFileAndForget( assetPath )
+					.LoadSerializedFileAndForget( AssetPath )
 					.OfType<T>()
 					.FirstOrDefault()
 				;
@@ -62,31 +67,23 @@ namespace Kogane
 		public static SettingsProvider CreateSettingsProvider
 		(
 			[CanBeNull] string                   settingsProviderPath = null,
-			[CanBeNull] string                   assetPath            = null,
-			[CanBeNull] Action<SerializedObject> onGUI                = null
+			[CanBeNull] Action<SerializedObject> onGUI                = null,
+			[CanBeNull] Action<SerializedObject> onGUIExtra           = null
 		)
 		{
-			// Project Settings のパスが指定されていない場合はデフォルト値を使用する
 			if ( settingsProviderPath == null )
 			{
 				settingsProviderPath = $"Kogane/{typeof( T ).Name}";
 			}
 
-			// .asset のファイルパスが指定されていない場合はデフォルト値を使用する
-			if ( assetPath == null )
-			{
-				assetPath = $"ProjectSettings/Kogane/{typeof( T ).Name}.asset";
-			}
-
 			// ScriptableObject のインスタンスを新規作成もしくは .asset から読み込む
 			// ScriptableObject の GUI を表示する SettingsProvider を作成する
-			var instance         = CreateOrLoadFromAsset( assetPath );
+			var instance         = GetInstance();
 			var serializedObject = new SerializedObject( instance );
 			var keywords         = SettingsProvider.GetSearchKeywordsFromSerializedObject( serializedObject );
-			var editor           = Editor.CreateEditor( instance );
 			var provider         = new SettingsProvider( settingsProviderPath, SettingsScope.Project, keywords );
 
-			provider.guiHandler += _ => OnGuiHandler( editor, assetPath, onGUI );
+			provider.guiHandler += _ => OnGuiHandler( onGUI, onGUIExtra );
 
 			return provider;
 		}
@@ -96,11 +93,13 @@ namespace Kogane
 		/// </summary>
 		private static void OnGuiHandler
 		(
-			Editor                   editor,
-			string                   assetPath,
-			Action<SerializedObject> onGUI
+			Action<SerializedObject> onGUI,
+			Action<SerializedObject> onGUIExtra
 		)
 		{
+			var instance = GetInstance();
+			var editor   = Editor.CreateEditor( instance );
+
 			using ( var scope = new EditorGUI.ChangeCheckScope() )
 			{
 				var serializedObject = editor.serializedObject;
@@ -118,13 +117,15 @@ namespace Kogane
 					editor.DrawDefaultInspector();
 				}
 
+				onGUIExtra?.Invoke( serializedObject );
+
 				if ( !scope.changed ) return;
 
 				// パラメータが編集された場合は インスタンスに反映して
 				// なおかつ .asset ファイルとしても保存する
 				serializedObject.ApplyModifiedProperties();
 
-				var directoryPath = Path.GetDirectoryName( assetPath );
+				var directoryPath = Path.GetDirectoryName( AssetPath );
 
 				if ( !string.IsNullOrWhiteSpace( directoryPath ) )
 				{
@@ -134,7 +135,7 @@ namespace Kogane
 				InternalEditorUtility.SaveToSerializedFileAndForget
 				(
 					obj: new[] { editor.target },
-					path: assetPath,
+					path: AssetPath,
 					allowTextSerialization: true
 				);
 			}
